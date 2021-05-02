379e3bbb8c5faee33cfd1f8163c26e3ca9e8eee9
hassasraf <hassasraf@gmail.com>
2021-05-02 23:18:35 +0600

    Edited js code

---
+++ app.js
@@ -0,0 +1,5 @@
+const toggleBtn = document.querySelector('.toggleBtn');
+const links = document.querySelector('.links');
+toggleBtn.addEventListener('click', ()=>{
+    links.classList.toggle('show');
+})
